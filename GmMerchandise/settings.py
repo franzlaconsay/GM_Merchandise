78889f2317a1f8443687362f64cc9b24619dbb94
franzlaconsay <70378747+franzlaconsay@users.noreply.github.com>
2021-05-16 09:45:59 +0800

    Update settings.py

--- GmMerchandise/settings.py
+++ GmMerchandise/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lg!57u2xdgd%s#5475@hii3qsge^_%y9+sv$e-yh1b*4^@+050'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
